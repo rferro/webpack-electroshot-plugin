123c9cc0557a2c5c295937d854a7444cea34adee
rferro <ricardo.ferro@gmail.com>
2017-09-14 22:22:27 -0300

    Create index.js.

---
+++ index.js
@@ -0,0 +1,75 @@
+
+const fs = require('fs')
+const path = require('path')
+
+const _ = require('lodash')
+const execa = require('execa')
+
+export default class WebpackElectroshotPlugin {
+  constructor (options) {
+    this.options = Object.assign({}, {
+      delay: null,
+      format: 'jpg',
+      out: null,
+      resolution: '1280',
+      selector: null,
+      url: 'index.html',
+      zoomFactor: null
+    }, options)
+  }
+
+  apply (compiler) {
+    const outputPath = compiler.options.output.path
+
+    compiler.plugin('after-emit', (compilation, cb) => {
+      let cmd = ['electroshot']
+
+      for (let option in this.options) {
+        let valid = ['resolution', 'url'].indexOf(option) === -1
+        let value = this.options[option]
+
+        if (option === 'out') {
+          value = value === null
+            ? path.resolve(outputPath)
+            : path.resolve(outputPath, value)
+        }
+
+        if (!valid || value === null) {
+          continue
+        }
+
+        cmd.push(`--${_.kebabCase(option)}${value === true ? '' : ` ${value}`}`)
+      }
+
+      cmd.push(`"file://${outputPath}/${this.options.url}"`)
+      cmd.push(`"${this.options.resolution}"`)
+
+      let outputFilePath = path.join(
+        this.options.out ? this.options.out : '',
+        this.options.filename
+      )
+
+      let outputFile = path.resolve(
+        outputPath,
+        outputFilePath
+      )
+
+      let outputDir = path.dirname(outputFile)
+
+      if (!fs.existsSync(outputDir)) {
+        fs.mkdirSync(outputDir)
+      }
+
+      execa.shell(cmd.join(' '))
+        .then((data) => {
+          console.log('electroshot', outputFilePath, data.stdout)
+        })
+        .catch((err) => {
+          console.log('electroshot', outputFilePath, err.message)
+        })
+        .then(() => {
+          cb()
+        })
+    })
+  }
+}
